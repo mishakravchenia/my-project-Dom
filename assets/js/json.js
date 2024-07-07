1bd2ed063e24e04cee4a672c495009eb362190a1
mishakravchenia <mihailokravchenia@gmail.com>
2024-07-07 11:18:33 +0300

    Add JSON

---
+++ assets/js/json.js
@@ -0,0 +1,34 @@
+// "use strict";
+
+// const FACTSCAT = "https://catfact.ninja/fact";
+
+// function updateFactCat() {
+//   fetch(FACTSCAT)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const catFactElement = document.getElementById("cat-fact");
+//       catFactElement.innerHTML = `${data.fact} (${data.length} characters)`;
+//     });
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const button = document.getElementById("bt");
+//   button.addEventListener("click", updateFactCat);
+// });
+const FACTSCAT_URL = "https://catfact.ninja/fact";
+
+function fetchCatFact() {
+  fetch(FACTSCAT_URL)
+    .then((response) => response.json())
+    .then((data) => displayCatFact(data));
+}
+
+function displayCatFact(factData) {
+  const catFactElement = document.getElementById("cat-fact");
+  catFactElement.innerHTML = `${factData.fact} (${factData.length} characters)`;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("bt");
+  button.addEventListener("click", fetchCatFact);
+});
